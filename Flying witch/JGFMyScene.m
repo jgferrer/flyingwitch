deebb14f21c018ab1c43456f8018446d2fe57d83
José Gabriel Ferrer <jgferrer@MBPr2013.local>
2014-06-13 18:58:39 +0200

    Primer Commit.

--- Flying witch/JGFMyScene.m
+++ Flying witch/JGFMyScene.m
@@ -8,46 +8,141 @@
 
 #import "JGFMyScene.h"
 
-@implementation JGFMyScene
+#define TIME 1.5
+#define MINIMUM_PILLER_HEIGHT 50.0f
+#define GAP_BETWEEN_TOP_AND_BOTTOM_PILLER 100.0f
 
--(id)initWithSize:(CGSize)size {    
+#define UPWARD_PILLER @"Upward_Green_Pipe"
+#define Downward_PILLER @"Downward_Green_Pipe"
+
+static inline CGPoint CGPointAdd(const CGPoint a, const CGPoint b)
+{
+    return CGPointMake(a.x + b.x, a.y + b.y);
+}
+
+static inline CGPoint CGPointMultiplyScalar(const CGPoint a, const CGFloat b)
+{
+    return CGPointMake(a.x * b, a.y * b);
+}
+
+@interface JGFMyScene() <SKPhysicsContactDelegate>
+{
+    NSTimeInterval _dt;
+    float bottomScrollerHeight;
+}
+@property (nonatomic) SKSpriteNode* backgroundImageNode;
+@property (nonatomic) SKSpriteNode* player;
+@property (nonatomic) NSArray* playerFlyingFrames;
+
+@property (nonatomic) float BG_VEL;
+
+@property (nonatomic) NSTimeInterval lastSpawnTimeInterval;
+@property (nonatomic) NSTimeInterval lastUpdateTimeInterval;
+@end
+
+@implementation JGFMyScene
+{}
+
+-(id)initWithSize:(CGSize)size
+{
     if (self = [super initWithSize:size]) {
+        
+        _BG_VEL = (TIME * 60);
+        
         /* Setup your scene here */
+        [self initalizingScrollingBackground];
         
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
+        [self initializePlayer];
         
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-        
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
-        
-        [self addChild:myLabel];
     }
     return self;
 }
 
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
+-(void)initializePlayer
+{
+    NSMutableArray *flyingFrames = [NSMutableArray array];
+    SKTextureAtlas *playerAnimatedAtlas = [SKTextureAtlas atlasNamed:@"witch"];
+    int numImages = playerAnimatedAtlas.textureNames.count;
+    for (int i=0; i < numImages/2; i++) {
+        NSString *textureName = [NSString stringWithFormat:@"%d", i];
+        SKTexture *temp = [playerAnimatedAtlas textureNamed:textureName];
+        [flyingFrames addObject:temp];
+    }
+    _playerFlyingFrames = flyingFrames;
     
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
+    SKTexture *temp = _playerFlyingFrames[0];
+    _player = [SKSpriteNode spriteNodeWithTexture:temp];
+    
+    //_player.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
+    _player.position = CGPointMake(30, CGRectGetMidY(self.frame));
+    
+    [self addChild:_player];
+    [self flyingPlayer];
+}
+
+
+-(void)flyingPlayer
+{
+    //This is our general runAction method to make our bear walk.
+    [_player runAction:[SKAction repeatActionForever:
+                      [SKAction animateWithTextures:_playerFlyingFrames
+                                       timePerFrame:0.07f
+                                             resize:NO
+                                            restore:YES]] withKey:@"FlyingInPlacePlayer"];
+    return;
+}
+
+-(void)initalizingScrollingBackground
+{
+    for (int i = 0; i < 2; i++)
+    {
+        SKSpriteNode *bg = [SKSpriteNode spriteNodeWithImageNamed:@"background.png"];
+        bottomScrollerHeight = bg.size.height;
+        bg.position = CGPointMake(i * bg.size.width, 0);
+        bg.anchorPoint = CGPointZero;
+        bg.name = @"bg";
         
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
+        [self addChild:bg];
     }
 }
 
--(void)update:(CFTimeInterval)currentTime {
-    /* Called before each frame is rendered */
+- (void)moveBackground
+{
+    [self enumerateChildNodesWithName:@"bg" usingBlock: ^(SKNode *node, BOOL *stop)
+     {
+         SKSpriteNode * bg = (SKSpriteNode *) node;
+         CGPoint bgVelocity = CGPointMake(-_BG_VEL, 0);
+         CGPoint amtToMove = CGPointMultiplyScalar(bgVelocity,_dt);
+         bg.position = CGPointAdd(bg.position, amtToMove);
+         
+         //Checks if bg node is completely scrolled of the screen, if yes then put it at the end of the other node
+         if (bg.position.x <= -bg.size.width)
+         {
+             bg.position = CGPointMake(bg.position.x + bg.size.width*2,
+                                       bg.position.y);
+         }
+         
+         [bg removeFromParent];
+         [self addChild:bg];        //Ordering is not possible. so this is a hack
+         _player.zPosition = bg.zPosition + 1;
+     }];
+}
+
+- (void)update:(NSTimeInterval)currentTime
+{
+    if (self.lastUpdateTimeInterval)
+    {
+        _dt = currentTime - _lastUpdateTimeInterval;
+    }
+    else
+    {
+        _dt = 0;
+    }
+    //CFTimeInterval timeSinceLast = currentTime - self.lastUpdateTimeInterval;
+    self.lastUpdateTimeInterval = currentTime;
+    
+    [self moveBackground];
+    
 }
 
 @end
--- Flying witch/JGFViewController.m
+++ Flying witch/JGFViewController.m
@@ -8,20 +8,32 @@
 
 #import "JGFViewController.h"
 #import "JGFMyScene.h"
+#import <iAd/iAd.h>
+
+@interface JGFViewController () <ADBannerViewDelegate>
+    @property (nonatomic, strong) ADBannerView *adView;
+@end
 
 @implementation JGFViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
 
+- (void)viewWillLayoutSubviews
+{
+    [super viewWillLayoutSubviews];
+    
+    _adView = [[ADBannerView alloc] initWithFrame:CGRectZero];
+    _adView.delegate = self;
+    [_adView setFrame:CGRectMake(0, 0, 1024, 768)]; // set to your screen dimensions
+    [self.view addSubview:_adView];
+    
+    
     // Configure the view.
     SKView * skView = (SKView *)self.view;
-    skView.showsFPS = YES;
-    skView.showsNodeCount = YES;
+    //skView.showsFPS = YES;
+    //skView.showsNodeCount = YES;
     
     // Create and configure the scene.
-    SKScene * scene = [JGFMyScene sceneWithSize:skView.bounds.size];
+    SKScene * scene = [JGFMyScene sceneWithSize:CGSizeMake(skView.bounds.size.width,skView.bounds.size.height)];
     scene.scaleMode = SKSceneScaleModeAspectFill;
     
     // Present the scene.
@@ -42,10 +54,31 @@
     }
 }
 
+- (BOOL)prefersStatusBarHidden {
+    return YES;
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Release any cached data, images, etc that aren't in use.
+}
+
+- (void)bannerView:(ADBannerView *)banner didFailToReceiveAdWithError:(NSError *)error
+{
+    if (banner.isBannerLoaded) {
+        [UIView beginAnimations:@"animateAdBannerOff" context:NULL];
+        // Assumes the banner view is placed at the bottom of the screen.
+        banner.frame = CGRectOffset(banner.frame, 0, banner.frame.size.height);
+        [UIView commitAnimations];
+    }
+}
+
+-(void)bannerViewDidLoadAd:(ADBannerView *)banner {
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:1];
+    [banner setAlpha:1];
+    [UIView commitAnimations];
 }
 
 @end
